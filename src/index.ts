49ccd9c71e862127ab29964996651e9f64e49c90
Bhagyaraj-B-K <bhagyarajbk7@gmail.com>
2025-09-20 15:00:15 +0530

    UPDATE: - Add index.ts for exports - Update tsconfig.json for declaration settings - Remove private flag from package.json

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Decorators
+export * from './decorators/route.decorator.js';
+export * from './decorators/body-dto.decorator.js';
+export * from './decorators/query-dto.decorator.js';
+export * from './decorators/response-dto.decorator.js';
+export * from './decorators/middleware.decorator.js';
+export * from './decorators/websocket.decorator.js';
+
+// Utils
+export * from './utils/autobind.util.js';
+export * from './utils/exceptions.util.js';
+export * from './utils/logger.util.js';
+export * from './utils/openapi.util.js';
+export * from './utils/typecast.util.js';
